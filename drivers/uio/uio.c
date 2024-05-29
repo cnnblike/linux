7f870362aae6fff52c3f771db91e822de852c054
Ke Li <cnnblike@gmail.com>
2024-05-29 10:50:34 +0800

    jailhouse enabling

--- drivers/uio/uio.c
+++ drivers/uio/uio.c
@@ -849,6 +849,15 @@ static int uio_mmap(struct file *filep, struct vm_area_struct *vma)
 		ret = -EINVAL;
 		goto out;
 	}
+	
+	if (idev->info->mem[mi].readonly) {
+		if (vma->vm_flags & VM_WRITE) {
+			ret = -EINVAL;
+			goto out;
+		}
+
+		vm_flags_clear(vma, VM_MAYWRITE);
+	}
 
 	if (idev->info->mmap) {
 		ret = idev->info->mmap(idev->info, vma);
